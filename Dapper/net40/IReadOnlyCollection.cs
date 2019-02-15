eac4944851bdbd05bfe230ecb1ec225619256280
Sun John <hd2y@outlook.com>
2019-02-15 10:35:25 +0800

    compatible with net40

---
+++ Dapper/net40/IReadOnlyCollection.cs
@@ -0,0 +1,18 @@
+﻿#if NET40
+using System;
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// 表示元素的强类型化只读集合。
+    /// </summary>
+    public interface IReadOnlyCollection<out T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// 获取集合中的元素数。
+        /// </summary>
+        int Count { get; }
+    }
+} 
+#endif
---
+++ Dapper/net40/IReadOnlyDictionary.cs
@@ -0,0 +1,41 @@
+﻿#if NET40
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// 表示键/值对的泛型只读集合。
+    /// </summary>
+    public interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>>
+    {
+        /// <summary>
+        /// 确定只读字典是否包含具有指定的键的元素
+        /// </summary>
+        bool ContainsKey(TKey key);
+
+        /// <summary>
+        /// 获取与指定键关联的值
+        /// </summary>
+        bool TryGetValue(TKey key, out TValue value);
+
+        /// <summary>
+        /// 获取只读字典中具有指定的键的元素
+        /// </summary>
+        TValue this[TKey key] { get; }
+
+        /// <summary>
+        /// 获取包含只读字典中的键的可枚举集合
+        /// </summary>
+        IEnumerable<TKey> Keys { get; }
+
+        /// <summary>
+        /// 获取包含只读字典中的值的可枚举集合
+        /// </summary>
+        IEnumerable<TValue> Values { get; }
+    }
+} 
+#endif
